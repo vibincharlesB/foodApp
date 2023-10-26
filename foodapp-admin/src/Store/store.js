b233ab3672a9a0d42525a2c4335230aa24de6413
vibincharlesB <vibincharles.b@infognana.com>
2023-10-26 16:17:48 +0530

    initial setup

--- foodapp-admin/src/Store/store.js
+++ foodapp-admin/src/Store/store.js
@@ -1,7 +1,7 @@
 // store.js
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk'; // Import Redux Thunk middleware
-import rootReducer from './reducers'; // Replace with your root reducer
+import rootReducer from './Reducers/index'
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
