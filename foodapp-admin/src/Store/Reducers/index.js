d4e6ab5560cd7088b0d272cb6d811a3c321a7250
vibincharlesB <vibincharles.b@infognana.com>
2023-10-26 17:04:24 +0530

    initial setup

--- foodapp-admin/src/Store/Reducers/index.js
+++ foodapp-admin/src/Store/Reducers/index.js
@@ -3,8 +3,7 @@ import { combineReducers } from 'redux';
 // import userReducer from './userReducer'; // Import your reducer(s)
 
 const rootReducer = combineReducers({
-  // user: userReducer, // Add your reducer(s) here
-  // Add more reducers as needed
+  // user: userReducer, 
 });
 
 export default rootReducer;
